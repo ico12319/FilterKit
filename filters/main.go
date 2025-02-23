9123c456aab70728cd5492a7ed54c78ca54c47b5
ico12319 <hristokubev@gmail.com>
2025-02-23 12:34:47 +0200

    add unit tests for crafting directories

--- filters/main.go
+++ filters/main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	startTime := time.Now()
-	filter4 := craftIntersectionFilter.CreateIntersectionFilter("allowed.txt", "blocked.txt", 6)
+	filter4 := craftIntersectionFilter.CreateIntersectionFilter("allowed3.txt", "blocked3.txt", 6)
 	fmt.Println(filter4.Accepts("gopher"))
 	fmt.Println(time.Since(startTime))
 }
