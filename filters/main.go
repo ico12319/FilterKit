d3b3bd45295f1305bc33dddeea074430d8ace5f8
ico12319 <hristokubev@gmail.com>
2025-02-23 15:53:17 +0200

    mocking

--- filters/main.go
+++ filters/main.go
@@ -10,5 +10,5 @@ func main() {
 	startTime := time.Now()
 	filter4 := craftIntersectionFilter.CreateIntersectionFilter("allowed3.txt", "blocked3.txt", 6)
 	fmt.Println(filter4.Accepts("gopher"))
-	fmt.Println(time.Since(startTime)) //for measuring performance
+	fmt.Println(time.Since(startTime))
 }
